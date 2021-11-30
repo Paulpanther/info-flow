bb75080b2f0c2fea55fb9629bcadcacc3f745793
Paul Methfessel <paul.methfessel@web.de>
2021-11-30 16:46:10 +0100

    Add bfs

--- rumor-detection.py
+++ rumor-detection.py
@@ -1,23 +1,36 @@
 import math
+import random
+from collections import deque
 
 
 def make_graph(n):
-    pass
+    return {i: random.sample(list(range(n)), random.randint(0, n)) for i in range(n)}
 
 
-if __name__ == '__main__':
+def bfs(adjs, root):
+    queue = deque([root])
+    visited = {root: True}
+    T = {}
 
+    while len(queue) > 0:
+        v = queue.pop()
+        visited[v] = True
+        children = adjs[v]
+        T[v] = []
+
+        for child in children:
+            if not visited.get(child, False):
+                T[v].append(child)
+                queue.appendleft(child)
+    return T
+
+
+def rumor_centrality(adjs, root):
     t = {}
     p = {}
     r = {}
-    adjs = {
-        0: [1],
-        1: [2],
-        2: [],
-    }
     visited = {}
 
-    root = 0
     n = len(adjs)
 
     def dfs_up(v):
@@ -40,6 +53,7 @@ if __name__ == '__main__':
         t[v] = current_t
         p[v] = current_t * current_p
 
+
     def dfs_down(v):
         children = adjs[v]
         visited[v] = True
@@ -57,9 +71,27 @@ if __name__ == '__main__':
 
     dfs_down(root)
 
-    current_max_e = root
-    for v in adjs:
-        if r[v] > r[current_max_e]:
-            current_max_e = v
+    # current_max_e = root
+    # for v in adjs:
+    #     if r[v] > r[current_max_e]:
+    #         current_max_e = v
+    #
+    # return current_max_e, r[current_max_e]
 
-    print(current_max_e, r[current_max_e])
+    return r[root]
+
+
+if __name__ == '__main__':
+    g = make_graph(10)
+
+    max_i = 0
+    max_r = 0
+    for v in g:
+        T = bfs(g, v)
+        r = rumor_centrality(T, v)
+        if r > max_r:
+            max_r = r
+            max_i = v
+    print(max_i, max_r)
+
+
