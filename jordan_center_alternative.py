2c225c1e64ca9d7f870db8173e93dd2d26b1084b
Tim Garrels <tim.garrels@protonmail.com>
2021-12-28 19:22:41 +0100

    Cleaner Documentation of Distance Centrality Scores

--- jordan_center_alternative.py
+++ jordan_center_alternative.py
@@ -1,37 +1,32 @@
+"""Calculation of infection centers based on centrality scores"""
+
 import networkx
-from networkx.generators.random_graphs import dense_gnm_random_graph
 from networkx.algorithms.distance_measures import center
 from networkx.algorithms.centrality import betweenness_centrality, closeness_centrality
-
-from graph_visualization import plot_nx_graph
-
-
-def random_graph(n, m, seed=None) -> networkx.Graph:
-    return dense_gnm_random_graph(n, m, seed)
+from typing import List
 
 
-def centers_by_jordan_center(g):
+def centers_by_jordan_center(g: networkx.Graph) -> List[int]:
+    """Infection centers by jordan centrality measurement"""
     return center(g)
 
 
-def centers_by_betweenness_centrality(g):
+def centers_by_betweenness_centrality(g: networkx.Graph) -> List[int]:
+    """Infection centers by betweenness centrality measurement"""
     b_c_dict = betweenness_centrality(g)
-    top = sorted(b_c_dict.items(), key=lambda x: x[1], reverse=True)[0]
-    return top
+    top_betweenness_centrality = sorted(b_c_dict.items(), key=lambda x: x[1], reverse=True)[0][1]
+    return [node for node, score in b_c_dict.items() if score == top_betweenness_centrality]
 
 
-def centers_by_distance_centrality(g):
+def centers_by_distance_centrality(g: networkx.Graph) -> List[int]:
+    """Infection centers by distance centrality measurement"""
     d_c_dict = closeness_centrality(g)
-    top = sorted(d_c_dict.items(), key=lambda x: x[1], reverse=True)[0]
-    return top
+    top_distance_centrality = sorted(d_c_dict.items(), key=lambda x: x[1], reverse=True)[0][1]
+    return [node for node, score in d_c_dict.items() if score == top_distance_centrality]
 
 
-def main():
-    # nodes = 200
-    # edges = 300
-    # g = random_graph(nodes, edges)
+def test():
     g = networkx.karate_club_graph()
-    plot_nx_graph(g)
 
     print("centers_by_jordan_center")
     print(centers_by_jordan_center(g))
@@ -42,4 +37,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    test()
